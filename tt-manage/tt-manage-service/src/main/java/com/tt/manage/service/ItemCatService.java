65a04b8dca5084bce1a1696e8c46e329bcf3eac1
cnhjh_admin <916129767@qq.com>
2018-06-01 17:09:19 +0800

    Service封装

--- tt-manage/tt-manage-service/src/main/java/com/tt/manage/service/ItemCatService.java
+++ tt-manage/tt-manage-service/src/main/java/com/tt/manage/service/ItemCatService.java
@@ -1,24 +1,32 @@
 package com.tt.manage.service;
 
-import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.github.abel533.mapper.Mapper;
 import com.tt.manage.mapper.ItemCatMapper;
 import com.tt.manage.pojo.ItemCat;
 
 @Service
-public class ItemCatService {
+public class ItemCatService extends BaseService<ItemCat>{
 
-	@Autowired
-	private ItemCatMapper itemCatMapper;
+//	@Autowired
+//	private ItemCatMapper itemCatMapper;
 
-	public List<ItemCat> queryItemCatListByParentId(Long pid) {
-		ItemCat record = new ItemCat();
-		record.setParentId(pid);//查询参数
-		return this.itemCatMapper.select(record);
-	}
+//	@Override
+//	public Mapper<ItemCat> getMapper() {
+//		return this.itemCatMapper;
+//	}
+
+//	public List<ItemCat> queryItemCatListByParentId(Long pid) {
+//		ItemCat record = new ItemCat();
+//		record.setParentId(pid);//查询参数
+//		return this.itemCatMapper.select(record);
+//	}
+
+
+	
+	
 	
 	
 }
--- tt-manage/tt-manage-web/src/main/java/com/tt/manage/controller/ItemCatController.java
+++ tt-manage/tt-manage-web/src/main/java/com/tt/manage/controller/ItemCatController.java
@@ -27,7 +27,11 @@ public class ItemCatController {
 	@RequestMapping(method = RequestMethod.GET)
 	public ResponseEntity<List<ItemCat>> queryItemCatListByParentId(@RequestParam(value = "id" ,defaultValue = "0")Long pid){
 		try {
-			List<ItemCat> list = this.itemCatService.queryItemCatListByParentId(pid);
+			//List<ItemCat> list = this.itemCatService.queryItemCatListByParentId(pid);//封装BaseServcie前的用法
+			
+			ItemCat record = new ItemCat();
+			record.setParentId(pid);
+			List<ItemCat> list = this.itemCatService.queryListByWhere(record);
 			if(null == list || list.isEmpty()){
 				return ResponseEntity.status(HttpStatus.NOT_FOUND).body(null);
 			}
